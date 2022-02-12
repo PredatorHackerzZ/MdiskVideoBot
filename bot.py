814f154586a1e51cc62effdf754b9fd67bb6cc07
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-02-12 22:24:06 +0530

    Create bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def start(bot, message):
 @bot.on_message(filters.text & filters.private)
 async def Doodstream_uploader(bot, message):
     new_string = str(message.text)
-    conv = await message.reply("Ruko jara Sabar kro ✋")
+    conv = await message.reply("Processing your given URL ⚡")
     dele = conv["message_id"]
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
@@ -162,8 +162,8 @@ async def remove_username(new_List):
 
 async def addFooter(str):
     footer = """
-
-""" + CHANNEL + """ """
+━━━━━━━━━━━━━━━
+✪ »JOIN CHANNEL ➡️ t.me/""" + CHANNEL
     return str + footer
 
 bot.run()
